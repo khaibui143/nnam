a9054e0eb7f5c75dcee3b6ba02543a48db61290c
Pham Khac Dat <kdat519@gmail.com>
2021-12-09 19:59:02 +0700

    Add random

--- scripts/base.js
+++ scripts/base.js
@@ -33,10 +33,30 @@ document.querySelector(".filter").addEventListener("change", function () {
   iso.arrange({ filter: filterItem });
 });
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+const list = Array.from(Array(4).keys());
+list.shift();
+shuffle(list);
 let infScroll = new InfiniteScroll(grid, {
   path: function () {
-    if (this.loadCount < 3) {
-      return `items/p${this.loadCount + 1}.html`;
+    if (this.loadCount < list.length) {
+      return `items/p${list[this.loadCount]}.html`;
     }
   },
   history: false,
